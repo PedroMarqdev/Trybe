b94ed3bc08d2223a355764ac3a4feda9eb57c923
PedroMarqDev <PedroMarqDev@gmail.com>
2020-08-27 18:35:00 -0300

    Exercício do triângulo

---
+++ trybe-exercises/Introdução ao Desenvolvimento Web/bloco_4/day1/ex-angulostriangulo.js
@@ -0,0 +1,12 @@
+let angulo1 = 55;
+let angulo2 = 45;
+let angulo3 = 90;
+let triangulotru = false;
+
+if ((angulo1 + angulo2 + angulo3) == 180){
+    console.log("É um triângulo sim, rapaix!");
+    triangulotru = true;
+}
+else {
+    console.log("Num é triangulozinho, poxa")
+}
