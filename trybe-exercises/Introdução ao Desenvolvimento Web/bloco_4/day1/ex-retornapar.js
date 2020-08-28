41d55da721ee4ca69fc4b6baaf6686e4ad793f4f
PedroMarqDev <PedroMarqDev@gmail.com>
2020-08-28 09:52:45 -0300

    Exercicio par

---
+++ trybe-exercises/Introdução ao Desenvolvimento Web/bloco_4/day1/ex-retornapar.js
@@ -0,0 +1,11 @@
+let numero1 = 1;
+let numero2 = 2;
+let numero3 = 3;
+let parzinho = false;
+if (numero1 % 2 == 0 || numero2 % 2 == 0 || numero3 % 2 == 0) {
+    parzinho = true;
+    console.log(parzinho);
+}
+else {
+    console.log(parzinho);
+}
