4c1bd9917ef72ed031e9236ce29a80e78b2571a9
PedroMarqDev <PedroMarqDev@gmail.com>
2020-08-28 17:53:36 -0300

    Exercício de média

---
+++ trybe-exercises/Introdução ao Desenvolvimento Web/bloco_4/day2/ex-somaarraymedia.js
@@ -0,0 +1,7 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let somarray = 0;
+for (index = 0; index < numbers.length; index += 1){
+  somarray += numbers[index]
+}
+let mediarray = somarray / numbers.length;
+console.log(`A média do array é: ${mediarray}`);
