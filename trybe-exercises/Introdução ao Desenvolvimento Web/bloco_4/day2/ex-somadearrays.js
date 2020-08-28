ddb6af6de8e10a5934461550378114979ee30772
PedroMarqDev <PedroMarqDev@gmail.com>
2020-08-28 17:51:55 -0300

    Exercício 2 soma

---
+++ trybe-exercises/Introdução ao Desenvolvimento Web/bloco_4/day2/ex-somadearrays.js
@@ -0,0 +1,6 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let somarray = 0;
+for (index = 0; index < numbers.length; index += 1){
+  somarray += numbers[index]
+}
+console.log(`A soma do array é: ${somarray}`);
