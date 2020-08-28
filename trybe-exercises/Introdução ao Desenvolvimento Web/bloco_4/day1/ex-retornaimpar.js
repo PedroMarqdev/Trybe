34917974507a6dbe0ad26218798a948720fb40a5
PedroMarqDev <PedroMarqDev@gmail.com>
2020-08-28 09:54:57 -0300

    Exercício impar

---
+++ trybe-exercises/Introdução ao Desenvolvimento Web/bloco_4/day1/ex-retornaimpar.js
@@ -0,0 +1,11 @@
+let numero1 = 1;
+let numero2 = 2;
+let numero3 = 4;
+let imparzinho = false;
+if (numero1 % 2 != 0 || numero2 % 2 != 0 || numero3 % 2 != 0) {
+    imparzinho = true;
+    console.log(imparzinho);
+}
+else {
+    console.log(imparzinho);
+}
