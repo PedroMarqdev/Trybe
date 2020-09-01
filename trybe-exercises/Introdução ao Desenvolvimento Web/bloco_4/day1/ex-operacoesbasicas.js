c140544716bd68357c3abb9d8d06f0131f15ac55
PedroMarqDev <PedroMarqDev@gmail.com>
2020-09-01 16:07:23 -0300

    Adicionado funções as operações básicas

--- trybe-exercises/Introdução ao Desenvolvimento Web/bloco_4/day1/ex-operacoesbasicas.js
+++ trybe-exercises/Introdução ao Desenvolvimento Web/bloco_4/day1/ex-operacoesbasicas.js
@@ -2,26 +2,38 @@ let x = 5;
 let y = 10;
 
 // Soma
+function soma(x, y) {
+    return x + y;
+}
 
-let soma = x + y
-console.log("A soma dessas variáveis é "+ soma);
+// console.log('A soma dessas variáveis é + $[soma()]';
 
 // Subtração
 
-let subtracao = x - y;
-console.log("A subtração dessas variáveis é " + subtracao);
+function subtracao(x, y) {
+    return x - y;
+}
+
+// console.log("A subtração dessas variáveis é " + subtracao);
 
 // Multiplicação
 
-let multiplicacao = x * y;
-console.log("A subtração dessas variáveis é " + multiplicacao);
+function multiplicacao(x, y){
+    return x * y;
+}
+
+// console.log("A subtração dessas variáveis é " + multiplicacao);
 
 // Divisão
 
-let divisao = x / y;
-console.log("A divisão dessas variáveis é " + divisao);
+function divisao(x, y){
+    return x / y;
+}
+// console.log("A divisão dessas variáveis é " + divisao);
 
 // Módulo
 
-let modulo = x % y;
-console.log("O módulo dessas variáveis é " + modulo);
+function modulo(x, y) {
+    return x % y;
+}
+// console.log("O módulo dessas variáveis é " + modulo);
