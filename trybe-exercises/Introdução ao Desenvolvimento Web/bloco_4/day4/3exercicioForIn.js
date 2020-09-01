6b8396e62477bdb832be9c121157e1711ef6c47c
PedroMarqDev <PedroMarqDev@gmail.com>
2020-09-01 18:37:06 -0300

    3 exercício e mudança no 2

--- trybe-exercises/Introdução ao Desenvolvimento Web/bloco_4/day4/3exercicioForIn.js
+++ trybe-exercises/Introdução ao Desenvolvimento Web/bloco_4/day4/3exercicioForIn.js
@@ -3,6 +3,7 @@ let info = {
     origem: "Pato Donald",
     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
   };
-
-  info["recorrente"] = "Sim!"
-  console.log(info)
+   info["recorrente"] = "Sim!"
+  for(let key in info) {
+      console.log(key)
+  }
