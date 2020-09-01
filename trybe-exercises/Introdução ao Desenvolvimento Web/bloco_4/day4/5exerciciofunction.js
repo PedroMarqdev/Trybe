f16cfe92f8c36beba5cdeed35d6ec12e0eec8306
PedroMarqDev <PedroMarqDev@gmail.com>
2020-09-01 20:48:04 -0300

    5 exercício e correção

--- trybe-exercises/Introdução ao Desenvolvimento Web/bloco_4/day4/5exerciciofunction.js
+++ trybe-exercises/Introdução ao Desenvolvimento Web/bloco_4/day4/5exerciciofunction.js
@@ -1,7 +1,19 @@
-function somaateN(N){
-    let somatorio = 0;
-    for(i = 0; i <= N; i += 1) {
-        somatorio += i;
-    }
-    return somatorio;
+function repeticao(array){
+    let numeros = array
+    let acertosnovos = 0;
+    let acertosantigos = 0;
+    let maiornumerorep = 0;
+   for(i = 0; i < numeros.length; i += 1){
+       for(j = 1; j < numeros.length; j += 1){
+        if(numeros[i] == numeros[j]){
+            acertosnovos += 1
+        }
+     }
+     if(acertosnovos > acertosantigos){
+         acertosantigos = acertosnovos;
+         maiornumerorep = numeros[i]
+         acertosnovos = 0;
+     }
+   }
+   return maiornumerorep
 }
---
+++ trybe-exercises/Introdução ao Desenvolvimento Web/bloco_4/day4/6exerciciofunction.js
@@ -0,0 +1,7 @@
+function somaateN(N){
+    let somatorio = 0;
+    for(i = 0; i <= N; i += 1) {
+        somatorio += i;
+    }
+    return somatorio;
+}
