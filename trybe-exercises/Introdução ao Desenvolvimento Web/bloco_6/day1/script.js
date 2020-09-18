324584c680d903c5ac158f569e566987e2236c82
PedroMarqDev <PedroMarqDev@gmail.com>
2020-09-18 13:32:46 -0300

    Terminado o form

--- trybe-exercises/Introdução ao Desenvolvimento Web/bloco_6/day1/script.js
+++ trybe-exercises/Introdução ao Desenvolvimento Web/bloco_6/day1/script.js
@@ -1,4 +1,8 @@
 const cidades = document.querySelector(".estados");
+const data = document.querySelector(".data");
+const submitButton = document.querySelector("#submitButton");
+const eraseButton = document.querySelector("#erase");
+let dataverify = false;
 let contador = 0;
 let cityObject = {
   AC: "Acre",
@@ -31,13 +35,62 @@ let cityObject = {
 };
 
 for (key in cityObject) {
-  let option = document.createElement('option');
+  let option = document.createElement("option");
   option.innerText = cityObject[key];
-  document.querySelector('.estados').appendChild(option);
+  document.querySelector(".estados").appendChild(option);
 }
 
-for(index = 0; index < 27; index += 1){
-    let options = document.querySelectorAll('.estados option')
-    options[contador].value = Object.getOwnPropertyNames(cityObject)[index]
-    contador += 1
+for (index = 0; index < 27; index += 1) {
+  let options = document.querySelectorAll(".estados option");
+  options[contador].value = Object.getOwnPropertyNames(cityObject)[index];
+  contador += 1;
 }
+
+data.addEventListener("change", function () {
+  const texto = data.value.split("/");
+  const dia = parseInt(texto[0]);
+  const mes = parseInt(texto[1]);
+  const ano = parseInt(texto[2]);
+  if (dia < 0 || dia > 31 || mes < 0 || mes > 12 || ano < 0) {
+    dataverify = false;
+  } else {
+    dataverify = true;
+  }
+});
+
+function verifiedData() {
+  if (dataverify === false) {
+    alert(
+      "Provavelmente você colocou um dia, mês ou ano invalido, ou simplesmente não usou o formato dd/mm/aa"
+    );
+  }
+}
+
+submitButton.addEventListener("click", function (evt) {
+  const inputs = document.querySelectorAll("input");
+  const textArea = document.querySelectorAll('textarea')
+  evt.preventDefault();
+  const divResult = document.createElement("div");
+  divResult.classList.add('resultado')
+  document.body.appendChild(divResult);
+  verifiedData();
+  if (dataverify === true) {
+    inputs.forEach(key => {
+      const elementDiv = document.createElement("p");
+      elementDiv.innerText = key.name + ": " + key.value;
+      divResult.appendChild(elementDiv);
+    });
+    textArea.forEach(key => {
+      const elementDiv = document.createElement("p");
+      elementDiv.innerText = key.name + ": " + key.value;
+      divResult.appendChild(elementDiv);
+    });
+  }
+});
+
+eraseButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    const divResult = document.querySelector('.resultado')
+     divResult.remove()
+     window.location.reload(true)
+})
