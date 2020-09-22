4e81145db744a116f7bdbe4f781246c648c708ae
PedroMarqDev <PedroMarqDev@gmail.com>
2020-09-22 10:20:46 -0300

    Adicionado forms

--- trybe-exercises/Introdução ao Desenvolvimento Web/bloco_6/day2/script.js
+++ trybe-exercises/Introdução ao Desenvolvimento Web/bloco_6/day2/script.js
@@ -45,51 +45,15 @@ for (index = 0; index < 27; index += 1) {
   options[contador].value = Object.getOwnPropertyNames(cityObject)[index];
   contador += 1;
 }
-data.addEventListener("change", function () {
-  const texto = data.value.split("/");
-  const dia = parseInt(texto[0]);
-  const mes = parseInt(texto[1]);
-  const ano = parseInt(texto[2]);
-  if (dia < 0 || dia > 31 || mes < 0 || mes > 12 || ano < 0) {
-    dataverify = false;
-  } else {
-    dataverify = true;
-  }
-});
-
-function verifiedData() {
-  if (dataverify === false) {
-    alert(
-      "Provavelmente você colocou um dia, mês ou ano invalido, ou simplesmente não usou o formato dd/mm/aa"
-    );
-  }
-}
-
-submitButton.addEventListener("click", function (evt) {
-  const inputs = document.querySelectorAll("input");
-  const textArea = document.querySelectorAll('textarea')
-  evt.preventDefault();
-  const divResult = document.createElement("div");
-  divResult.classList.add('resultado')
-  document.body.appendChild(divResult);
-  verifiedData();
-  if (dataverify === true) {
-    inputs.forEach(key => {
-      const elementDiv = document.createElement("p");
-      elementDiv.innerText = key.name + ": " + key.value;
-      divResult.appendChild(elementDiv);
-    });
-    textArea.forEach(key => {
-      const elementDiv = document.createElement("p");
-      elementDiv.innerText = key.name + ": " + key.value;
-      divResult.appendChild(elementDiv);
-    });
-  }
-});
 
 eraseButton.addEventListener("click", function (evt) {
     evt.preventDefault();
     const divResult = document.querySelector('.resultado')
-     divResult.remove()
-     window.location.reload(true)
+        if (divResult){
+          divResult.remove()
+          window.location.reload(true)
+        }
+        else{
+          window.location.reload(true)
+        }
 })
