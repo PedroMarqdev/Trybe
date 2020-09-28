8140955423f6ac8de35a372a7d359a81fd81c08c
PedroMarqDev <PedroMarqDev@gmail.com>
2020-09-28 18:20:12 -0300

    ex1

---
+++ trybe-exercises/Introdução ao Desenvolvimento Web/bloco_7/day1/ex2.js
@@ -0,0 +1,24 @@
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+  const exercise1 = (object, prop, value) => {
+      return object[prop] = value;
+  }
+exercise1()
+console.log(lesson2)
