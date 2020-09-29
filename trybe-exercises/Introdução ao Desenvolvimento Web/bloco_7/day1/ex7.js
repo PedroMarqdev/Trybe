51ec86fcfbe38e9dbc5ec1cda10b5b6353a3efd2
PedroMarqDev <PedroMarqDev@gmail.com>
2020-09-29 10:35:23 -0300

    ex7

---
+++ trybe-exercises/Introdução ao Desenvolvimento Web/bloco_7/day1/ex7.js
@@ -0,0 +1,43 @@
+// 6 - Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo
+// console.log(getValueByNumber(lesson1, 0));
+// Output: 'Matématica'
+
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+    turno: 'manhã'
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+  const allLessons = {
+      lesson1: {},
+      lesson2: {},
+      lesson3: {},
+  };
+
+  const exercise5 = () => {
+      const lessons = [lesson1, lesson2, lesson3]
+      for(key in lessons){
+          Object.assign(allLessons, {lesson1}, {lesson2}, {lesson3})
+      }
+      }
+
+      exercise5();
+
+  const exercise7 = (lesson, key) => Object.values(lesson)[key];
+
+
