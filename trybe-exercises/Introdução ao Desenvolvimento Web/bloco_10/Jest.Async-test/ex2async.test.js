5eecdb2a83a7dd1397dddfed8b948fcbc86a807c
PedroMarqDev <PedroMarqDev@gmail.com>
2020-10-29 19:20:28 -0300

    Async jest 1, 2 e 3

--- trybe-exercises/Introdução ao Desenvolvimento Web/bloco_10/Jest.Async-test/ex2async.test.js
+++ trybe-exercises/Introdução ao Desenvolvimento Web/bloco_10/Jest.Async-test/ex2async.test.js
@@ -1,26 +1,30 @@
-const { users, findUserById, getUserName} = require('../Jest.Async/ex2async')
+const { users, findUserById, getUserName } = require("../Jest.Async/ex2async");
 
-it('User exists', () => {
-   return expect(getUserName(4)).resolves.toEqual('Mark')
-})
+describe("Check if the user exist", () => {
+  it("User exists", () => {
+    return expect(getUserName(4)).resolves.toEqual("Mark");
+  });
 
-it("User doesn't exists", () => {
-    return expect(getUserName(6)).rejects.toEqual(
-        { error: 'User with ' + '6' + ' not found.' })
- })
+  it("User exists await", async (done) => {
+    const user = await getUserName(4);
+    expect.assertions(1);
+    expect(user).toBe("Mark");
+    done();
+  });
+});
 
-it('User exists await', async (done) => {
-    const user = await getUserName(4)
-    expect.assertions(1)
-    expect(user).toBe('Mark')
-    done()
-})
-
-it("User doesn't exists", async () => {
+describe("Check if the doesn't user exist", () => {
+  it("User doesn't exists await", async () => {
     try {
-        await getUserName('Fofo')
+      await getUserName("Fofo");
+    } catch (error) {
+      expect(error).toEqual({ error: "User with " + "Fofo" + " not found." });
     }
-    catch (error) {
-        expect(error).toEqual({ error: 'User with ' + 'Fofo' + ' not found.' })
-    }
- })
+  });
+
+  it("User doesn't exists", () => {
+    return expect(getUserName(6)).rejects.toEqual({
+      error: "User with " + "6" + " not found.",
+    });
+  });
+});
