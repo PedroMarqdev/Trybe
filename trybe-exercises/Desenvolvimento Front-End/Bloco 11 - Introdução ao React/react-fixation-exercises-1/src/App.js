4490d86e803b39793e73559b2bab0a7ee5ebfb15
PedroMarqDev <PedroMarqDev@gmail.com>
2020-11-10 16:04:21 -0300

    Other fixs

--- trybe-exercises/Desenvolvimento Front-End/Bloco 11 - Introdução ao React/react-fixation-exercises-1/src/App.js
+++ trybe-exercises/Desenvolvimento Front-End/Bloco 11 - Introdução ao React/react-fixation-exercises-1/src/App.js
@@ -5,7 +5,7 @@ import Image from './image'
 function App() {
   return (
     <div>
-      <Image src='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alt='Cute cat staring' />
+      <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jp' alternativeText='Cute cat staring' />
     </div>
   )
   }
